5091e8d2389553e470f6ab46c4c362a90f1a02cb
mattyf9x <mdfonner@gmail.com>
2024-03-06 01:15:04 -0600

    added getter and setter

--- BankApp/src/BankAccount.java
+++ BankApp/src/BankAccount.java
@@ -1,8 +1,34 @@
 import java.util.Scanner;
+/*
+ * Use classes to encapsulate account information, ensuring that sensitive data is private and accessed through getters and setters that perform validation (OBJ02-J, OBJ03-J). 
 
+Prevent heap pollution by using generics with specific types for account collections (OBJ03-J). 
+
+Avoid using public static nonfinal fields, ensuring that account information is not globally accessible or modifiable (OBJ10-J). 
+ */
 public class BankAccount {
     private String accountHolder;
     private double balance;
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+
+    public String getAccountHolder()
+    {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String newAccountHolder)
+    {
+        accountHolder = newAccountHolder;
+    }
     
 }
 
