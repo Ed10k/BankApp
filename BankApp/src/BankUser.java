e645901ef2ddfec20f48d2222166e5f2c058cdeb
Lane <LTPlummer96@gmail.com>
2024-03-11 16:54:24 -0500

    First rough draft for BankUser methods and attributes

--- BankApp/src/BankUser.java
+++ BankApp/src/BankUser.java
@@ -1,3 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankUser {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private List<BankAccount> accounts;
+
+    public BankUser(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.accounts = new ArrayList<>();
+    }
     
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+
+    public List<BankAccount> getAccounts() {
+        return accounts;
+    }
+
+    public void addAccount(BankAccount account) {
+        accounts.add(account);
+        System.out.println("Account added successfully.");
+    }
 }
