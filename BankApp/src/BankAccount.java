ec3e42dfcce02b3fdbb832ccaf8c91b2aedbe46e
mattyf9x <mdfonner@gmail.com>
2024-03-14 03:43:39 -0500

    removed some comments

--- BankApp/src/BankAccount.java
+++ BankApp/src/BankAccount.java
@@ -1,19 +1,4 @@
 /*
- * Use classes to encapsulate account information, ensuring that sensitive data is private and accessed through getters and setters that perform validation (OBJ02-J, OBJ03-J). 
-
-Prevent heap pollution by using generics with specific types for account collections (OBJ03-J). 
-
-Avoid using public static nonfinal fields, ensuring that account information is not globally accessible or modifiable (OBJ10-J). 
- */
-
- /*
-  * OBJ01-J. Limit accessibility of fields
-    OBJ02-J. Preserve dependencies in subclasses when changing superclasses
-    OBJ03-J. Prevent heap pollution
-    OBJ10-J. Do not use public static nonfinal fields
-  */
-
-/*
  * This is the BankAccount interface that will be implemented in a checking account and savings account
  */
 public interface BankAccount {
