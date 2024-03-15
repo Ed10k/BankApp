1799d7551f16eb8682c374f4b388923263dc3aec
iangowland22 <iangowland22@gmail.com>
2024-03-14 21:07:07 -0500

    Creation of verification class

---
+++ BankApp/src/Verification.java
@@ -0,0 +1,33 @@
+
+/**
+ * Class description
+ */
+
+public class Verification {
+
+    /*
+     * Rules:
+     * 
+     * EXP00-J. Do not ignore values returned by methods 
+     * 
+     * FIO05-J. Do not expose buffers or their backing arrays methods to untrusted code 
+     * FIO08-J. Distinguish between characters or bytes read from a stream and -1
+     * 
+     * IDS00-J.
+     * IDS01-J. Normalize strings before validating them. 
+     * 
+     * NUM07-J. Do not attempt comparisons with NaN. 
+     * NUM00-J: Detect or Prevent Integer Overflow 
+     * 
+     * MET00-J. Validate Method Arguments 
+     * 
+     * SEC01-J
+     * SEC03-J 
+     * 
+     * SER03-J. Do not serialize unencrypted sensitive data.
+     * 
+     * VNA05-J. Ensure atomicity when reading and writing 64-bit values.
+     * 
+     */
+
+}
