c0937fbe4959a22f7cf4e851e5b4330c3fa6438c
Joe Edozie <Josephedozie45@gmail.com>
2024-03-16 13:46:24 -0500

    Made updates to the BankAPP class. The choice function is a function compried of all the possible switch cases (currently). Made updates to the BankUser class as well to have a username and password as well for the sake of verification.

--- BankApp/src/BankUser.java
+++ BankApp/src/BankUser.java
@@ -4,16 +4,39 @@ import java.util.List;
 public class BankUser {
     private String firstName;
     private String lastName;
+    private String username;
+    private String password;
     private int age;
     private List<BankAccount> accounts;
 
-    public BankUser(String firstName, String lastName, int age) {
+    public BankUser(String firstName, String lastName, int age, String username, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.username = username;
         this.age = age;
         this.accounts = new ArrayList<>();
     }
+
+    public void setUsername(String username){
+        this.username = username;
+        
+    }
     
+    public String getUsername(){
+        return username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+        
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+
+
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
