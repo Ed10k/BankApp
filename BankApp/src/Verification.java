6fafc3c1a965d0bc0cd72572b255b23a895fb36d
Joe Edozie <Josephedozie45@gmail.com>
2024-03-17 18:07:02 -0500

    updated ians class

--- BankApp/src/Verification.java
+++ BankApp/src/Verification.java
@@ -1,55 +1,43 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.Normalizer;
 import java.text.Normalizer.Form;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 /**
- * Class description
+ * This class is a compilation of several rules that have been implemented and 
+ * that can be called statically to verify safe operations, exception handling
+ * and exception messages, etc. 
+ * 
+ * Rules referenced in this class:
+ * IDS01-J. This class will take a String object and normalize it prior to validation.
+ * NUM07-J. Direct comparisons should never happen with NaN (not a number). 
+ * EXP01-J: Don’t use a Null in a case where an Object is required.
+ * ERR01-J. Do not allow exceptions to expose sensitive information. 
+ * OBJ03-J. Prevent heap pollution.
  * 
  * @author Ian Gowland
  */
 
 public class Verification {
 
-    /*
-     * Rules:
-     * 
-     * FIO05-J. Do not expose buffers or their backing arrays methods to untrusted code 
-     * FIO08-J. Distinguish between characters or bytes read from a stream and -1
-     * 
-     * IDS00-J.
-     * IDS01-J. Normalize strings before validating them. 
-     * 
-     * NUM07-J. Do not attempt comparisons with NaN. 
-     * NUM00-J: Detect or Prevent Integer Overflow 
-     * 
-     * MET00-J. Validate Method Arguments 
-     * 
-     * SER03-J. Do not serialize unencrypted sensitive data.
-     * Set the password variable, whether in this class or in User to transient.
-     * Tbis will prevent the JVM from serializing the data. 
-     * Set the password variable, whether in this class or in User to transient.
-     * Tbis will prevent the JVM from serializing the data. 
-     * 
-     * VNA05-J. Ensure atomicity when reading and writing 64-bit values.
-     * 
-     * ERR01-J. Do not allow exceptions to expose sensitive information.
-     * /
-     
+    private static final String SENTINEL = "*";
 
-    /**
-     * IDS01-J. This class will take a String object and normalize it prior to validation.
-     * Doing so will prevent the possibility of a user including a script tag in their String.
-     *
-     * The emphasis of normalization would prevent a user from using a script tag. 
-     * Without the normalization, there would be no possibility of this user executing a script. 
-     * This scripting is called cross-site scripting (XSS) and can cause a user to unknowingly execute 
-     * a malicious script that can expose their information or the website's information.
-     * 
-     * @param str - String to check or convert to normalized String.
-     * @return normalStr - Return string that has been normalized.
-     */
+     /**
+      * IDS01-J. This class will take a String object and normalize it prior to validation.
+      * Doing so will prevent the possibility of a user including a script tag in their String.
+      *
+      * The emphasis of normalization would prevent a user from using a script tag. 
+      * Without the normalization, there would be no possibility of this user executing a script. 
+      * This scripting is called cross-site scripting (XSS) and can cause a user to unknowingly execute 
+      * a malicious script that can expose their information or the website's information.
+      * 
+      * @param str - String to check or convert to normalized String.
+      * @return normalStr - Return string that has been normalized.
+      */
      public static String normalizeString(String str) {
         String normalStr = Normalizer.normalize(str, Form.NFKC);
         // Validate
@@ -72,18 +60,152 @@ public class Verification {
       * comparisons could cause unintended behaviors, so it must be confirmed that any 
       * mathematical operation does not directly compare to NaN. This is especially true
       * when working with floating point numbers. 
-      
-      * @param flo - Float variable to be checked if NaN
-      * @return - If the original variable is NaN, it gets
-      reassigned to null. If it's not, it just return the 
-      original number.
+      *
+      * EXP01-J: Don’t use a Null in a case where an Object is required. This rule is
+      * expressed within this method in the case that the argument is an instance of NaN.
+      * Instead of assigning the variable to null, assign to 0 instead to prevent the 
+      * potential NullPointerException. 
+      *
+      * @param dub - Double variable to be checked if NaN
+      * @return - If the original variable is NaN, it gets reassigned to null. If it's not, 
+      * it just returns the original number.
       */
-     public static Float verifyFloatNonNaN(Float flo) {
-        if (Float.isNaN(flo)) {
+     public static Double verifyDoubleNonNaN(Double dub) {
+        if (Double.isNaN(dub)) {
             System.out.println("Result is NaN.");
-            flo = null;
+            dub = 0.0;
         }
-        return flo;
+        return dub;
+     }
+
+     /**
+      * ERR01-J. Do not allow exceptions to expose sensitive information. 
+      * 
+      * Whenever programming a try catch, this method can be called to handle exceptions.
+      * Certain messages should not be displayed as they can expose sensitive infomration
+      * about the server, but even the client as well. So anytime try catch blocks are 
+      * used in the program, pass in the Exception object where the exception can prevent
+      * having its message displayed. 
+      * 
+      * @param except - Encountered Exception, which will be assessed based on its Exception type.
+      */
+     public static boolean handleExceptions(Exception except) {
+
+        if(except instanceof FileNotFoundException) {
+            System.out.println("File not found exception occurred.");
+            return false;
+        } else if (except instanceof IOException) {
+            System.out.println("IOException occured.");
+            return false;
+        // Add else if for any other risky exception and it's corresponding getMessage
+        } else {
+            // Exception message can be displayed.
+            System.out.println(except.getMessage());
+        }
+        return true;
+     }
+
+     /**
+      * OBJ03-J. Prevent heap pollution.
+      * 
+      * Heap pollution occurs when a raw list (superclass Object list) contains 
+      * different data types for it's elements. This method would verify all 
+      * elements in the list are the same, which will prevent heap pollution 
+      * from occurring.
+      * 
+      * @param list - list that will have it's elements data types verified
+      * @return - true if the list contains all elements of the same type
+      */
+     public static boolean verifyGenericList(ArrayList list) {
+
+        if(!list.isEmpty()) {
+            Object first = list.get(0);
+            for(int i = 0; i < list.size(); i++){
+                Object next = list.get(i);
+                if(!first.getClass().equals(next.getClass())) {
+                    return false;
+                }
+            }
+        }
+        return true;
+     }
+
+     /**
+      * This method prompts a user to enter their password until it is correct.
+      * @param user - BankUser, signifying the client that needs to verify their password.
+      */
+     public static boolean verifyPassword(BankUser user) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter your password: ");
+        String input = scan.nextLine();
+        // while(!input.equals(user.getPassword()) && !input.equals(SENTINEL))
+        while(!input.equals(user.getPassword()) && !input.equals(SENTINEL)) {
+            //System.out.print("Incorrect.\nPlease enter your password:  (SENTINEL: Enter * to terminate loop.)");
+            System.out.print("Incorrect.\nPlease enter your password: ");
+            input = scan.nextLine();
+        }
+        scan.close();
+        return true;
+     }
+
+     /**
+      * Validate that user input is an integer
+      * @param input - input to be validated as an integer
+      * @return - true if the user's input can be parsed into an int
+      */
+      public static boolean verifyInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+     }
+
+     /**
+      * Loop until user enters valid input to parse an integer
+      * @param input - user's initial input
+      * @return - true, once user has entered valid input for an integer
+      */
+     public static Integer loopForInteger(String input) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter an integer: ");
+        while(!verifyInteger(input)){
+            System.out.print("Incorrect.\nPlease enter an integer: ");
+            input = scan.nextLine();
+        }
+        scan.close();
+        return Integer.parseInt(input);
+     }
+
+     /**
+      * Validate that user input is a double
+      * @param input - input to be validated as a double
+      * @return - true if the user's input can be parsed into a double
+      */
+     public static boolean verifyDouble(String input) {
+        try {
+            Double.parseDouble(input);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+     }
+
+     /**
+      * Loop until user enters dollar input to parse a double
+      * @param input - user's initial input
+      * @return - true, once user has entered valid dollar (float) input for an integer
+      */
+     public static Double loopForDollarAmount(String input) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter a dollar amount: ");
+        while(!verifyDouble(input)){
+            System.out.print("Incorrect.\nPlease enter a dollar amount: ");
+            input = scan.nextLine();
+        }
+        scan.close();
+        return Double.parseDouble(input);
      }
 
      
